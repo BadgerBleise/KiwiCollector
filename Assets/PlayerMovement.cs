3108e280810e13bae00e66c6cd6168d617dfe5d5
DESKTOP-RRCDI28\muche <muchek200@gmail.com>
2023-03-21 00:06:02 +0000

    still player movement changes

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    
+    Rigidbody2D rb2d = null;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("space"))
+        {
+            rb2d.velocity = new Vector3(0, 7, 0);
+        }
     }
 }
