682ceea8bc9af36b13cdc1ce055a53c838bdd237
DESKTOP-RRCDI28\muche <muchek200@gmail.com>
2023-03-22 00:53:52 +0000

    Completing the game :P

--- Assets/CoinCollector.cs
+++ Assets/CoinCollector.cs
@@ -9,18 +9,13 @@ public class CoinCollector : MonoBehaviour
     public TMP_Text scoretext;
     public int score = 0;
 
-    void Update()
-    {
-        scoretext.text = "Kiwi Score: " + score.ToString();
-    }
-
-
 
    private void OnTriggerEnter2D(Collider2D kiwi) 
    {
      if(kiwi.gameObject.CompareTag("Kiwi"))
      {
         score++;
+        scoretext.text = "Kiwi Score: " + score.ToString();
         Destroy(kiwi.gameObject);
      }
    }
--- Assets/GameControl.cs
+++ Assets/GameControl.cs
@@ -7,13 +7,17 @@ using System;
 
 public class GameControl : MonoBehaviour
 {
-    public GameObject Text,Button;
+    public GameObject Text, Button;
     public CoinCollector Script;
     public TMP_Text TextContent;
     
     public void OnButtonClick()
     {
-        SceneManager.LoadScene("Level2");
+        // Store the current scene's build index in a variable
+        int sceneId = SceneManager.GetActiveScene().buildIndex;
+        Debug.Log(sceneId);
+        // Load the next scene using the sceneId variable
+        SceneManager.LoadScene(sceneId + 1); 
     }
     
     
@@ -21,18 +25,23 @@ public class GameControl : MonoBehaviour
     {
         if(other.gameObject.CompareTag("Finish"))
         {
-            
-            
             TextContent.text = "You have gathered " + Script.score.ToString() + " Kiwis";
-            
+
             Text.SetActive(true);
             Button.SetActive(true);
-
-
         }
-        
+         
+        if(other.gameObject.CompareTag("Spikes"))
+        {
+            RefreshScene();
+        }
     }
-    
-
-    
+    private void RefreshScene()
+    {
+         // Store the current scene's build index in a variable
+        int sceneId = SceneManager.GetActiveScene().buildIndex;
+         
+        // Load the next scene using the sceneId variable
+        SceneManager.LoadScene(sceneId); 
+    }
 }
